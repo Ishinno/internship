5d2cb4793a0df237808bab8a1acc9d60825d0b0f
Ishinno <arutema93@gmail.com>
2016-10-07 01:58:30 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,32 @@
+#tutorial_11~16
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "database" => "./bbs.db"
+)
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+class Comment < ActiveRecord::Base
+end
+
+get '/' do
+    @comments = Comment.order("id desc").all
+    erb :index
+end
+
+post '/new' do
+    Comment.create({:body => params[:body]})
+    redirect '/'
+    erb :index
+end
+
+post '/delete' do
+    Comment.find(params[:id]).destroy
+end
